587cab2efc12644ff70f3fd7004fff01d9973df3
fajarkarim <fajarabdulkarim@gmail.com>
2017-07-24 17:10:56 +0700

    authentication and authorization done and tested

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,6 +6,11 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/register', userCtrl.register)
 router.post('/login', userCtrl.login)
-router.get('/:id', helper.auth, userCtrl.ping)
+
+router.get('/', helper.auth, userCtrl.getAll)
+router.get('/:id', helper.auth, userCtrl.getOne)
+router.put('/:id', helper.auth, userCtrl.edit)
+router.delete('/:id', helper.auth, userCtrl.remove)
+
 
 module.exports = router;
